01e97c89e5413a021ef059604131cd6444a2ec81
Lucas Gabriel <lucasgabriel31032006@gmail.com>
2025-03-11 14:51:22 -0400

    Update Navbar.tsx

--- 5_auth/src/components/Navbar.tsx
+++ 5_auth/src/components/Navbar.tsx
@@ -1,5 +1,7 @@
-import { auth, signIn, signOut } from "auth";
+import { auth } from "auth";
 import Link from "next/link";
+import FormSignIn from "./FormSignIn";
+import FormSignOut from "./FormSignOut";
 
 async function Navbar() {
   const session = await auth();
@@ -13,34 +15,10 @@ async function Navbar() {
         {session && session.user ? (
           <div className="flex gap-4 items-center">
             <p>{session.user.name}</p>
-            <form
-              action={async () => {
-                "use server";
-                await signOut();
-              }}
-            >
-              <button
-                type="submit"
-                className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded"
-              >
-                Sair
-              </button>
-            </form>
+            <FormSignOut />
           </div>
         ) : (
-          <form
-            action={async () => {
-              "use server";
-              await signIn();
-            }}
-          >
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
-            >
-              Entrar
-            </button>
-          </form>
+          <FormSignIn />
         )}
       </div>
     </div>
